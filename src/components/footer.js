ffe9c5deabd8f294ad6d04422202d5d001fbfffe
vinay chaudhary <97957688+vinayofc@users.noreply.github.com>
2022-02-13 14:35:25 +0530

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,9 +6,9 @@ import * as styles from './footer.module.css'
 const Footer = () => (
   <Container as="footer">
     <div className={styles.container}>
-      Built with <a href="https://contentful.com/">Contentful</a> and{' '}
-      <a href="https://gatsbyjs.com">Gatsby</a> &middot;{' '}
-      <a href="https://github.com/contentful/starter-gatsby-blog">Source</a>
+      CMS with <a href="https://contentful.com/">Contentful</a> and{' '}
+      <a href="#">By</a> &middot;{' '}
+      <a href="https://github.com/vinayofc">Vinay Chaudhary</a>
     </div>
   </Container>
 )
